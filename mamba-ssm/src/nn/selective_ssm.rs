a81bebcfe1f8ff2f2c54a6652dff565ee676b34f
flawedmatrix <flawedmatrix@gmail.com>
2023-12-30 01:23:02 -0800

    Improve speed of selective scan
    By only realizing chunks of the SSM state one input at a time,
    we can avoid having to deal with tensors of size BLDN in memory
    and thus improve performance

--- mamba-ssm/src/nn/selective_ssm.rs
+++ mamba-ssm/src/nn/selective_ssm.rs
@@ -16,6 +16,7 @@ pub struct SSM {
     dt_proj: Linear,
 
     span: tracing::Span,
+    iter_span: tracing::Span,
 }
 
 impl SSM {
@@ -27,6 +28,7 @@ impl SSM {
         let d = vb.get(d_inner, "D")?;
 
         let span = tracing::span!(tracing::Level::TRACE, "SSM", d_inner, d_state, dt_rank);
+        let iter_span = tracing::span!(tracing::Level::TRACE, "iter");
 
         Ok(Self {
             dt_rank,
@@ -36,9 +38,55 @@ impl SSM {
             dt_proj,
 
             span,
+            iter_span,
         })
     }
+
+    fn selective_scan(
+        &self,
+        u: &Tensor,     // The input (batch_size, seq_len, d_inner) aka (B L D)
+        delta: &Tensor, // (batch_size, seq_len, d_inner) aka (B L D)
+        a: &Tensor,     // (d_inner, d_state) aka (D N)
+        b: &Tensor,     // (batch_size, seq_len, d_state) aka (B L N)
+        c: &Tensor,     // (batch_size, seq_len, d_state) aka (B L N)
+        d: &Tensor,     // (d_inner) aka (D)
+    ) -> Result<Tensor> {
+        let (batch_size, seq_len, d_inner) = u.dims3()?;
+        let d_state = a.dim(1)?;
+        let d = d.unsqueeze(1)?;
+
+        let mut bys = Vec::with_capacity(batch_size);
+        for batch in 0..batch_size {
+            let mut x = Tensor::zeros((d_inner, d_state), delta.dtype(), delta.device())?; // h_(t-1), size (D N)
+            let mut ys = Vec::with_capacity(seq_len);
+            for i in 0..seq_len {
+                let _enter_iter = self.iter_span.enter();
+                // We want to avoid carrying around BLDN elements in memory, so
+                // let's compute everything on the fly
+                let delta_i = delta.i((batch, i, ..))?.reshape((d_inner, 1))?;
+                let delta_a_i = delta_i.broadcast_mul(a)?.exp()?; // exp(Δ_i * A) (size D N)
+
+                let b_i = b.i((batch, i, ..))?.reshape((1, d_state))?;
+                let delta_b_i = delta_i.broadcast_mul(&b_i)?; // simplified discretization Δ_i * B (size D N)
+
+                let u_i = u.i((batch, i, ..))?.reshape((d_inner, 1))?;
+                let du_i = (&d * &u_i)?; // Calculate D * u (size D 1) first
+
+                let dbu_i = delta_b_i.broadcast_mul(&u_i)?; // ΔB_i * u_i (size D N)
+
+                x = ((delta_a_i * x)? + dbu_i)?; // h_t = (A_i * h_(t-1)) + (ΔB_i * u_i)
+
+                let c_i = c.i((batch, i, ..))?.reshape((d_state, 1))?;
+                let y = x.matmul(&c_i)?; // y = h_t * C (D N) x (N 1) -> (D 1)
+                ys.push((y + du_i)?)
+            }
+            let ys = Tensor::cat(&ys, 1)?; // [(D,1) x L] -> (D, L)
+            bys.push(ys)
+        }
+        Tensor::stack(&bys, 0)?.t()
+    }
 }
+
 impl Module for SSM {
     fn forward(&self, xs: &Tensor) -> Result<Tensor> {
         let _enter = self.span.enter();
@@ -55,33 +103,7 @@ impl Module for SSM {
         let delta = delta.contiguous()?.apply(&self.dt_proj)?;
         // softplus without threshold
         let delta = (delta.exp()? + 1.)?.log()?;
-        let ss = selective_scan(xs, &delta, &a, &b, &c, &d)?;
+        let ss = self.selective_scan(xs, &delta, &a, &b, &c, &d)?;
         Ok(ss)
     }
-}
-
-fn selective_scan(
-    u: &Tensor,
-    delta: &Tensor,
-    a: &Tensor,
-    b: &Tensor,
-    c: &Tensor,
-    d: &Tensor,
-) -> Result<Tensor> {
-    let (b_sz, l, d_in) = u.dims3()?;
-    let n = a.dim(1)?;
-    let delta = delta.t()?.reshape((b_sz, d_in, l, 1))?; // b d_in l 1
-    let delta_a = delta.broadcast_mul(&a.reshape((1, d_in, 1, n))?)?.exp()?;
-    let delta_b_u = delta
-        .broadcast_mul(&b.reshape((b_sz, 1, l, n))?)?
-        .broadcast_mul(&u.t()?.reshape((b_sz, d_in, l, 1))?)?;
-    let mut xs = Tensor::zeros((b_sz, d_in, n), delta_a.dtype(), delta_a.device())?;
-    let mut ys = Vec::with_capacity(l);
-    for i in 0..l {
-        xs = ((delta_a.i((.., .., i))? * xs)? + delta_b_u.i((.., .., i))?)?;
-        let y = xs.matmul(&c.i((.., i, ..))?.unsqueeze(2)?)?.squeeze(2)?;
-        ys.push(y)
-    }
-    let ys = Tensor::stack(ys.as_slice(), 1)?;
-    ys + u.broadcast_mul(d)
 }
